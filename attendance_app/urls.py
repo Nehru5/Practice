361f39bfb31dd76ef7b45196880034f52e5fc83e
Nehru raja <141004417+Nehru5@users.noreply.github.com>
2025-04-09 12:25:52 +0530

    Delete attendance_app directory

--- attendance_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('mark/<int:student_id>/<uuid:batch_uuid>/<int:trainer_id>/', views.mark_attendance, name='mark_attendance'),
-
-
-]
-
