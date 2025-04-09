73a1fdc5cfd670427b33e4e72eb5eb5d7ab5dcbb
Nehru raja <141004417+Nehru5@users.noreply.github.com>
2025-04-09 12:25:33 +0530

    Delete student_app directory

--- student_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('signup/', views.student_signup, name='student_signup'),
-    path('dashboard/', views.student_dashboard, name='student_dashboard'),
-    path('logout/', views.student_logout, name='student_logout'),
-    
-]
